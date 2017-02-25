bece694a3701e3face5e58397668031c815bb624
Florian Rinke <github+logcheckrules@florianrinke.de>
2017-02-25 16:20:08 +0100

    add negative-test and debug output to testscript

--- test.sh
+++ test.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
+#check failure detection
+egrep -f test/regex-fail /dev/null 2>/dev/null
+if [ $? != 2 ]; then 
+	echo "Test Error: Unknown regex behavior"
+	exit 2
+fi
+
 #check for regexp errors
 failflag=0
 basepath=$(git rev-parse --show-toplevel)
 cd $basepath/ignore.d.server
 for i in `ls`; do
-        egrep -f $i /dev/null 2>/dev/null
+        egrep -f $i /dev/null #2>/dev/null
         if [ $? -ne 1 ]; then
                 echo "error in server/$i"
                 failflag=1
@@ -14,7 +21,7 @@ done
 
 cd $basepath/ignore.d.workstation
         for i in `ls`; do
-                egrep -f $i /dev/null 2>/dev/null
+                egrep -f $i /dev/null #2>/dev/null
                 if [ $? -ne 1 ]; then
                         echo "error in workstation/$i"
                         failflag=1
