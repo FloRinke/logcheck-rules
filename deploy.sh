fa738c9316775f4f15fa7cf2fa8bb83826f1ea7f
Dominik Szmek <do@minik.pl>
2018-09-17 10:41:26 +0200

    add gitiginore and domino services

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 rsync -av --delete-after ignore.d.server ignore.d.workstation violations.ignore.d root@linode:/etc/logcheck/
+rsync -av --delete-after ignore.d.server ignore.d.workstation violations.ignore.d root@adm:/etc/logcheck/
+rsync -av --delete-after ignore.d.server ignore.d.workstation violations.ignore.d root@edyta.org.pl:/etc/logcheck/
