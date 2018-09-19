09352c61583bac91d70bb07bb152791efcff6292
Dominik Szmek <do@minik.pl>
2018-09-19 15:44:45 +0200

    modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-rsync -av --chown=root:logcheck ignore.d.server/ root@linode:/etc/logcheck/ignore.d.server
-rsync -av --chown=root:logcheck ignore.d.server/ root@linode2:/etc/logcheck/ignore.d.server
-rsync -av --chown=root:logcheck ignore.d.server/ root@adm:/etc/logcheck/ignore.d.server
-rsync -av --chown=root:logcheck ignore.d.server/ root@edyta.org.pl:/etc/logcheck/ignore.d.server
+HOSTS=(
+    linode2
+    linode
+    adm
+    edyta.org.pl
+)
+
+for host in ${HOSTS[*]}; do
+    echo "syncing ${host}"
+    rsync -av --chown=root:logcheck ignore.d.server/ root@${host}:/etc/logcheck/ignore.d.server
+done
+
+# rsync -av --chown=root:logcheck ignore.d.server/ root@linode2:/etc/logcheck/ignore.d.server
+# rsync -av --chown=root:logcheck ignore.d.server/ root@adm:/etc/logcheck/ignore.d.server
+# rsync -av --chown=root:logcheck ignore.d.server/ root@edyta.org.pl:/etc/logcheck/ignore.d.server
